616ca31fcfad15144cc122d758c0895bdc13d2f4
Athul-coder363 <74913344+Athul-coder363@users.noreply.github.com>
2021-09-26 14:09:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import { StyleSheet, Text, View } from "react-native"; 
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import ScanScreen from "./screens/ScanScreen"; 
+export default class App extends React.Component { 
+  render() {
+     return( 
+     <AppContainer />
+     );
+  }} 
+  var AppNavigator = createSwitchNavigator({
+    ScanScreen : ScanScreen
+  });
+  const AppContainer = createAppContainer(AppNavigator);
